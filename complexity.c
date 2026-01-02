f219d269de4d5c3ad36b527f265d805d83016239
Phantom <phantomboy866@gmail.com>
2026-01-02 17:46:04 +0530

    Initial DSA practice commit

---
+++ complexity.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n = 5;
+    int count = 0;
+
+    for (int i=0;i<n;i++)
+    {
+        count++;
+    }
+    printf("Count = %d\n",count);
+    return 0;
+}
---
+++ nested.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    /*int n = 5;
+    int count = 0;
+
+    for (int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            for(int k=0;k<n;k++)
+            {
+                count++;
+            }
+        }
+    }
+    printf("The count is : %d\n",count);
+
+    return 0;
+*/
+    int count = 0;
+    int x = count+++5;
+    printf("%d %d \n",count,x);
+    return 0;
+}
